2dbde85470b8ce38506e164fd8c25cefc56c1c28
Maksim Zozulya <kayubi@icloud.com>
2024-05-06 21:47:49 +0700

    Задание выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,44 +1,25 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let discriminant = b ** 2 - 4 * a * c;
-  if (discriminant === 0) {
-    let root = -b / ( 2 * a );
-    arr = [];
-    arr.push(+root.toFixed(2));
-  } else if (discriminant > 0) {
-    let root1 = ( -b + Math.sqrt(discriminant) ) / ( 2 * a );
-    let root2 = ( -b - Math.sqrt(discriminant) ) / ( 2 * a );
-    arr = [];
-    arr.push(+root1.toFixed(2), +root2.toFixed(2));
-  } else {
-    arr = [];
+  let d = b**2-4*a*c;
+  if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d))/(2*a);
+    arr[1] = (-b - Math.sqrt(d))/(2*a);
+  } else if (d == 0) {
+    arr[0] = -b/(2*a);
   }
-    return arr;
+  return arr;
 }
+solveEquation(1, 5, 4)
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let totalAmount;
-  let date;
-  percent = +percent;
-  contribution = +contribution;
-  amount = +amount;
-  date = +date;
-  if (typeof percent === `string`) {
-    totalAmount = `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
-  } else if (typeof contribution === `string`) {
-    totalAmount = `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
-  } else if (typeof amount === `string`) {
-    totalAmount = `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
-  } else if (typeof date === `string`) {
-    totalAmount = `Параметр "Сроки ипотеки" содержит неправильное значение ${date}`;
-  };
-  let lianAmount = amount - contribution;
-  let timeNow = new Date();
-  let loanTerm = date - timeNow;
-  loanTerm = loanTerm / 1000 / 60 / 60 / 24 / 30;
-  let monthlyPartPercent = (percent / 100) / 12;
-  let monthFee = lianAmount * (monthlyPartPercent + (monthlyPartPercent / (((1 + monthlyPartPercent) ** loanTerm) - 1)));
-  totalAmount = +(monthFee * loanTerm).toFixed(2);
-  console.log(totalAmount);
-  return totalAmount;
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+    return false;
+  }
+  let P = percent/100/12;
+  let S = amount - contribution;
+  let amountPerMonth = S * (P + (P / (((1 + P)**countMonths) - 1)));
+  let totalAmount = amountPerMonth * countMonths;
+  return Number(totalAmount.toFixed(2));
 }
+calculateTotalMortgage(10, 0, 10000, 36)
